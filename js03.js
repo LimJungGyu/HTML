fcdc26be6c7436b18fe4dd9d7c8903cb54a8bf45
LimJungGyu <97217412+LimJungGyu@users.noreply.github.com>
2022-02-15 15:23:17 +0900

    HTML실습

---
+++ js03.js
@@ -0,0 +1,9 @@
+var subject1 = "Welcome to Javascript<br>";
+var subject2 = "Enjoy Javascript Programming!<br>";
+
+function welcome(target){
+    document.write(target);
+}
+
+welcome(subject1);
+welcome(subject2);s
